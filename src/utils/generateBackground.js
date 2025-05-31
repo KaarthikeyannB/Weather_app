482eef30464a154e5123932c62ff652038018203
KaarthikeyannB <ks5748@srmist.edu.in>
2025-06-01 01:21:25 +0530

    path error

--- src/utils/generateBackground.js
+++ src/utils/generateBackground.js
@@ -1,18 +1,18 @@
 export const getBackgroundImage = (weather) => {
   switch (weather) {
     case "Clear":
-      return "/assets/clear-sky.webp";
+      return "assets/clear-sky.webp";
     case "Sunny":
-      return "/assets/clear-sky.webp";
+      return "assets/clear-sky.webp";
     case "Partly Cloudy":
-      return "/assets/Cloudy.jpg";
+      return "assets/Cloudy.jpg";
     case "Moderate or heavy rain with thunder":
-      return "/assets/rainy.webp";
+      return "assets/rainy.webp";
     case "Patchy rain nearby":
-      return "/assets/rainy.webp";
+      return "assets/rainy.webp";
     case "Light snow showers":
-      return "/assets/snow-images.jpg";
+      return "assets/snow-images.jpg";
     default:
-      return "/assets/default.jpg";
+      return "assets/default.jpg";
   }
 };
